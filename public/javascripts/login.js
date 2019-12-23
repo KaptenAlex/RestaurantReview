4a8a028ab5ec8c1e614601ac95f7b890d2781206
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-12-23 17:40:04 +0100

    added code to show and hide login/create account divs.

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,6 +1,11 @@
 $(function(){
   $("#createAccount").on("click", function(){
-    console.log("test");
+    $(".login-box").hide();
+    $(".createAccount-box").show();
+  });
+  $("#loginAccount").on("click", function(){
+    $(".createAccount-box").hide();
+    $(".login-box").show();
   });
   console.log("dom ready!");
 })
