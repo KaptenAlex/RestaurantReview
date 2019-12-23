4a0c3444ea4bebcbafac542b000a3d63c4d35cac
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-12-23 16:52:17 +0100

    Changed from index to login

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'RestaurantReview' });
+  res.render('login', { title: 'RestaurantReview' });
 });
 
 module.exports = router;
