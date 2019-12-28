82bfcb6396a3233d8860546a2ce960cfcc2551d0
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-12-28 12:17:07 +0100

    Added another query for restaurants and rendered users & restaurant into homepage.

--- routes/login.js
+++ routes/login.js
@@ -11,21 +11,37 @@ var connection = mysql.createConnection({
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('login', { title: 'RestaurantReview' });
+  res.render('login', {
+    title: 'RestaurantReview'
+  });
 });
 
-router.post("/home", function(req,res){
-  connection.connect(function(err){
+router.post("/home", function(req, res) {
+  connection.connect(function(err) {
     if (err) throw err;
+    let restaurants = [];
+    let userInfo = [];
     let username = req.body.name;
     let password = req.body.pass;
-    connection.query("SELECT * FROM `users` WHERE userName = ? AND userPassword = ?", [username,password], function(error,results,fields){
-      if(error) throw error;
-      console.log(results);
-      res.render('homepage', {title: 'RestaurantReview', userName: results[0].userName, role: results[0].roleID});
-      connection.end();
+    connection.query("SELECT * FROM `users` WHERE userName = ? AND userPassword = ?", [username, password], function(error, results) {
+      if (error) throw error;
+      connection.query("SELECT * FROM `restaurants`", function(error2, results2) {
+        if (error) throw error;
+        for (var i = 0; i < results.length; i++) {
+          console.log(results[i]);
+        }
+        for (var i = 0; i < results2.length; i++) {
+          console.log(results2[i]);
+        }
+        res.render('homepage', {
+          title: 'RestaurantReview',
+          user: results,
+          restaurants: results2
+        });
+      });
     });
   });
+  //connection.end();
 });
 
 module.exports = router;
