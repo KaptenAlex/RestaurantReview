d3173134ccc01113ebb30a20f87af09191cfff95
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-12-30 11:53:47 +0100

    Changed router name, added so the users created account loads after page render.

--- routes/register.js
+++ routes/register.js
@@ -12,14 +12,29 @@ var pool = mysql.createPool({
 });
 
 router.post("/createAccount", function(req, res) {
-  pool.getConnection(function(err) {
+  pool.getConnection(function(err, connection) {
     if (err) throw err;
     let username = req.body.username;
     let password = req.body.password;
     pool.query("INSERT INTO `users` SET userName = ?, userPassword = ?, roleID = ?", [username, password, 0], function(error, results, fields) {
       if (error) throw error;
-      console.log("Created user!");
-      res.render('homepage', { title: 'RestaurantReview' });
+      pool.query("SELECT * FROM `restaurants`", function(error2, results2) {
+        if (error2) throw error2;
+        for (var i = 0; i < results2.length; i++) {
+          console.log(results2[i]);
+        }
+        pool.query("SELECT * FROM `users` WHERE userName = ? AND userPassword = ?", [username, password], function(error3, results3) {
+          if (error) throw error;
+          for (var i = 0; i < results.length; i++) {
+            console.log(results[i]);
+          }
+          res.render('homepage', {
+            user: results3,
+            restaurants: results2,
+            title: 'RestaurantReview'
+          });
+        });
+      });
     });
     connection.release();
   });
