962d90d1adc5515c094b399bc5753b00872c4d46
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-12-31 15:55:17 +0100

    added body-parser to app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var homepageRouter = require('./routes/login');
 var registerRouter = require('./routes/register');
@@ -14,8 +15,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
