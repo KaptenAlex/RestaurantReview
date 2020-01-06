3b16fefb77e1ba9c54fc86e0a0f8a5efe52a09b9
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-01-06 11:17:09 +0100

    Added passport config.

--- config/passport.js
+++ config/passport.js
@@ -10,3 +10,42 @@ var pool = mysql.createPool({
   database: "restaurants",
   port: 10003
 });
+
+
+module.exports = (passport) => {
+  passport.use(new LocalStrategy({
+    usernameField: "uName",
+    passwordField: "passW"
+  }, (username, password, done) => {
+    pool.getConnection((error, connection) => {
+      if (error) throw error;
+      pool.query("SELECT * FROM `users` WHERE userName = ?", [username], (error2, user) => {
+        if (error2) throw error2;
+        if (!user) {
+          return done(null, false);
+        }
+        bcrypt.compare(password, user.userPassword, (err, auth) => {
+          if (err) throw err;
+          if (auth) {
+            return done(null, user);
+          } else {
+            return done(null, false);
+          }
+        });
+      });
+      connection.release();
+    });
+  }));
+  passport.serializeUser((user, done) => {
+    done(null, user.ID);
+  });
+  passport.deserializeUser((id, done) => {
+    pool.getConnection((error, connection) => {
+      pool.query("SELECT * FROM `users` WHERE ID = ?", [id], (err, user) => {
+        if (err) throw err;
+        done(err, user);
+      });
+      connection.release();
+    });
+  });
+}
