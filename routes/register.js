4f864edef5c67244885d09e11f29c09a26378f3e
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-01-08 16:06:02 +0100

    Added if statements to create account.

--- routes/register.js
+++ routes/register.js
@@ -13,24 +13,28 @@ var pool = mysql.createPool({
   port: 10003
 });
 
-//SQL queries
-let allRestaurants = "SELECT * FROM `restaurants`";
-let avgRatingForRestaurant = "SELECT AVG(rating) AverageScore,COUNT(r.rating) NumberOfRatings, r.restaurantID FROM `ratings` AS r JOIN `users` AS u ON r.userID = u.ID GROUP BY restaurantID";
-let topTenRatedRestaurants = "SELECT AVG(rating) averageScore,COUNT(r.rating) numberOfRatings, re.name FROM `ratings` AS r JOIN `users` AS u JOIN `restaurants` AS re ON r.userID = u.ID AND re.ID = r.restaurantID GROUP BY restaurantID ORDER BY averageScore DESC LIMIT 10";
-let noOfGenresAndWhich = "SELECT genre,COUNT(genre) AS noOfGenres FROM `restaurants` GROUP BY genre ORDER BY noOfGenres DESC";
-
 //Create new account
 router.post("/createAccount", async (req, res) => {
   try {
-    const hashPass = await bcrypt.hash(req.body.password, 10);
     pool.getConnection(function(err, connection) {
       if (err) throw err;
-      /*Need to add check to see if a user already exist with that username.
-        and send the user a message describing what went wrong.
-      */
-      pool.query("INSERT INTO `users` SET userName = ?, userPassword = ?, roleID = ?", [req.body.username, hashPass, 0], function(error, results) {
+      if (req.body.password.length < 1) {
+        res.redirect("../..");
+      } else if (req.body.username.length < 1) {
+        res.redirect("../..");
+      }
+      pool.query("SELECT userName FROM `users` where userName = ?", [req.body.username], async (error, result) => {
         if (error) throw error;
-        res.redirect("../..")
+        console.log(result.length);
+        if (result.length > 0) {
+          res.redirect("../..")
+        } else {
+          const hashPass = await bcrypt.hash(req.body.password, 10);
+          pool.query("INSERT INTO `users` SET userName = ?, userPassword = ?, roleID = ?", [req.body.username, hashPass, 0], function(error2, results) {
+            if (error2) throw error2;
+            res.redirect("../..")
+          });
+        }
       });
       connection.release();
     });
